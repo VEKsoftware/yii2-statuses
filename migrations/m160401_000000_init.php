df75df5a323897427cd6b1fdd453c522b461b02a
k1sliy <maks280795@mail.ru>
2016-04-11 15:10:59 +0500

    Init migration added

---
+++ migrations/m160401_000000_init.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+class m160401_000000_init extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('statuses_doctypes', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(64)->notNull(),
+            'symbolic_id' => $this->string(32)->notNull()->unique(),
+        ], null);
+
+        $this->createTable('statuses', [
+            'id' => $this->primaryKey(),
+            'doc_type' => $this->integer()->notNull(),
+            'name' => $this->string(64)->notNull()->unique(),
+            'description' => $this->string(128),
+            'symbolic_id' => $this->string(32)->notNull()->unique(),
+        ], null);
+
+        $this->addForeignKey('statuses_doc_type_fkey', 'statuses', 'doc_type', 'statuses_doctypes', 'id', 'CASCADE', 'CASCADE');
+
+        $this->createTable('statuses_links', [
+            'status_from' => $this->integer()->notNull(),
+            'status_to' => $this->integer()->notNull(),
+            'right_tag' => $this->string(64)->notNull()->unique(),
+        ], null);
+
+        $this->addForeignKey('statuses_links_statuses_id_fk1', 'statuses_links', 'status_from', 'statuses', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('statuses_links_statuses_id_fk2', 'statuses_links', 'status_to', 'statuses', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('reports_conditions');
+        $this->dropTable('reports');
+    }
+}
